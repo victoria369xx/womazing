f2c04c44d884b9f85d94960e83ebb6c771f55612
Victoria <v.shurkalina@gmail.com>
2023-02-14 22:08:47 +0400

    size filter, sort by price done

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -1,39 +1,79 @@
 import { useState } from 'react';
 import { ItemCard } from '../ItemCard/ItemCard';
-import { Pagination, Checkbox, FormControlLabel, Button } from '@mui/material';
+import { Pagination, Button } from '@mui/material';
 import { useSelector } from 'react-redux';
 import {getItemsList} from '../../store/itemsDB/selectors';
 import './index.scss'
+import banner from '../../assets/images/search_banner.jpg'; 
 
 
-export const ItemsList = (props) => {
 
-let sizes = ['XS', 'S', 'M', 'L', 'XL']
+export const ItemsList = () => {
+
+let sizes = ['Все','XS', 'S', 'M', 'L', 'XL']
+let prices = ['Дешевле', 'Дороже']
  let itemsList = useSelector(getItemsList)
- let [checkboxSizeValue, setCheckboxSizeValue] = useState('')
-
-let filteredItemsList =  itemsList.filter(item => {
-        return item.name.toLowerCase().includes(props.searchValue.toLowerCase())||item.brand.toLowerCase().includes(props.searchValue.toLowerCase())
+ let [filteredItemsList, setFilteredItemsList] = useState(itemsList)
+ let [searchValue, setSearchValue] = useState('')
+ 
+ const searchHandler = (event) => {
+        if (event.target.value === '') {
+            setFilteredItemsList(itemsList)
+            return
+        }
+    setSearchValue(event.target.value)
+    let filteredSearch = itemsList.filter(item => {
+        return item.name.toLowerCase().includes(searchValue.toLowerCase())||item.brand.toLowerCase().includes(searchValue.toLowerCase())
      })
-     
-     /*.filter(item => {
-        return item.size.includes(checkboxSizeValue)
-     })*/
- 
- 
-    return (
+     setFilteredItemsList(filteredSearch)
+ }
+
+const sizeFilterHandler = (value) => {
+    if (value === 'Все') {
+        setFilteredItemsList(itemsList)
+        return;
+    }
+    let filteredSize = filteredItemsList.filter(item => {
+        return item.size.includes(value)
+    })
+    setFilteredItemsList(filteredSize)
+}
+
+const priceFilterHandler = (value) => {
+    // .concat() используется, чтобы вернуть новый массив и сделать ре-рендер результата
+    if (value === 'Дешевле') {
+        let sortedCheaper = filteredItemsList.sort((a,b)=> a.price - b.price).concat()
+        setFilteredItemsList(sortedCheaper) 
+        return;
+    }
+
+    if (value === 'Дороже') {
+        let sortedMoreExpensive = filteredItemsList.sort((a, b) => b.price - a.price).concat()
+        setFilteredItemsList(sortedMoreExpensive)
+        return;
+    }
+}
+
+    return ( <> 
+      <section>
+            <div className="searchField" style={{ backgroundImage: `url(${banner})`, backgroundSize:'100%', backgroundRepeat:'no-repeat', height:'80vh'}}>
+                <input className='searchField-input' placeholder='Я ищу...' type='text' onChange={searchHandler}/>
+            </div> 
+        </section>
+
         <div style={{display:'flex', flexDirection:'column', alignItems:'center', marginBottom:'2rem'}}>
             <h2 className='itemsList-heading'> КАТАЛОГ </h2> 
             <div className='itemList-filters'>
                 <div className='filter-price filter-mr'>
                     <p><b>Цена:</b></p>  
-                    <Button sx={{color:'black', mr:2}}>Сначала дешевле</Button> 
-                    <Button sx={{color:'black'}}> Сначала дороже</Button>
+                    {prices.map((price,index)=> {
+                            return <Button key={index} onClick={()=>priceFilterHandler(price)} sx={{color:'black', mr:2}}>{price}</Button>
+                        })}
                 </div>
                 <div className='filter-size filter-mr'>
                     <p><b>Размер: </b></p>  
-                        {sizes.map((size, index)=> {
-                            return  <FormControlLabel control={<Checkbox value={size} onChange={event => setCheckboxSizeValue(event.target.value)}/>} label={size} key={index} />
+                        {sizes.map((size,index)=> {
+                            return <Button key={index} onClick={()=>sizeFilterHandler(size)} sx={{color:'black'}}>{size}</Button>
                         })}
                 </div>
             </div>
@@ -45,5 +85,6 @@ let filteredItemsList =  itemsList.filter(item => {
             
             <Pagination count={10} variant="outlined" />
         </div>
+        </>
     )
 }
